59059c03c9cfb7035200702bc3b14637ebeeaaab
qwert2603 <qwert2603@mail.ru>
2019-06-12 23:28:26 +0700

    example set_pattern / check_pattern

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,21 +12,95 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
+      routes: {
+        "check_pattern": (BuildContext context) => CheckPattern(),
+        "set_pattern": (BuildContext context) => SetPattern(),
+      },
       home: Scaffold(
         appBar: AppBar(
           title: Text("Pattern Lock Demo"),
         ),
-        body: new NewWidget(),
+        body: HomeWidget(),
       ),
     );
   }
 }
 
-class NewWidget extends StatelessWidget {
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  List<int> pattern;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: PatternLock(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          if (pattern != null) ...[
+            MaterialButton(
+              color: Colors.green,
+              child:
+                  Text("Check Pattern", style: TextStyle(color: Colors.white)),
+              onPressed: () async {
+                final result = await Navigator.pushNamed(
+                  context,
+                  "check_pattern",
+                  arguments: pattern,
+                );
+                if (result == true) {
+                  Scaffold.of(context).hideCurrentSnackBar();
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(
+                        "it's correct",
+                        style: TextStyle(color: Colors.green),
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+            SizedBox(height: 16),
+          ],
+          MaterialButton(
+            color: Colors.lightGreen,
+            child: Text("Set Pattern", style: TextStyle(color: Colors.white)),
+            onPressed: () async {
+              final result = await Navigator.pushNamed(context, "set_pattern");
+              if (result is List<int>) {
+                Scaffold.of(context).hideCurrentSnackBar();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(content: Text("pattern is $result")),
+                );
+                setState(() {
+                  pattern = result;
+                });
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CheckPattern extends StatelessWidget {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final List<int> pattern = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: Text("Check Pattern"),
+      ),
+      body: PatternLock(
         usedColor: Colors.red,
         pointRadius: 8,
         showInput: true,
@@ -35,18 +109,86 @@ class NewWidget extends StatelessWidget {
         relativePadding: 0.7,
         inputThreshold: 25,
         fillPoints: true,
-        onInputComplete: (points) {
-          final ok = listEquals<int>(points, [0, 3, 7]);
-          Scaffold.of(context).hideCurrentSnackBar();
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                ok ? "done" : "wrong",
-                style: TextStyle(color: ok ? Colors.green : Colors.red),
+        onInputComplete: (List<int> input) {
+          if (listEquals<int>(input, pattern)) {
+            Navigator.of(context).pop(true);
+          } else {
+            scaffoldKey.currentState.hideCurrentSnackBar();
+            scaffoldKey.currentState.showSnackBar(
+              SnackBar(
+                content: Text(
+                  "wrong",
+                  style: TextStyle(color: Colors.red),
+                ),
               ),
-            ),
-          );
+            );
+          }
         },
+      ),
+    );
+  }
+}
+
+class SetPattern extends StatefulWidget {
+  @override
+  _SetPatternState createState() => _SetPatternState();
+}
+
+class _SetPatternState extends State<SetPattern> {
+  bool isConfirm = false;
+  List<int> pattern;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(
+        title: Text("Check Pattern"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Flexible(
+            child: Text(
+              isConfirm ? "Confirm pattern" : "Draw pattern",
+              style: TextStyle(fontSize: 26),
+            ),
+          ),
+          Flexible(
+            child: PatternLock(
+              usedColor: Colors.amber,
+              pointRadius: 12,
+              onInputComplete: (List<int> input) {
+                if (isConfirm) {
+                  if (listEquals<int>(input, pattern)) {
+                    Navigator.of(context).pop(pattern);
+                  } else {
+                    scaffoldKey.currentState.hideCurrentSnackBar();
+                    scaffoldKey.currentState.showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          "Patterns do not match",
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                    );
+                    setState(() {
+                      pattern = null;
+                      isConfirm = false;
+                    });
+                  }
+                } else {
+                  setState(() {
+                    pattern = input;
+                    isConfirm = true;
+                  });
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
